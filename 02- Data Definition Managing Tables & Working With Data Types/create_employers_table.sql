1bb5692e1de5cbfa85f0ab5f10537cdada2e0236
imerla <imerla@imerla.com>
2023-11-14 04:05:50 -0800

    update SQL Script

--- 02- Data Definition Managing Tables & Working With Data Types/create_employers_table.sql
+++ 02- Data Definition Managing Tables & Working With Data Types/create_employers_table.sql
@@ -3,5 +3,7 @@ CREATE TABLE employers(
     company_address VARCHAR(300),
     -- yearly_revenue FLOAT(5, 2) Approximation
     yearly_revenue NUMERIC(5, 2), -- exact value
-    is_hiring BOOLEAN
+    is_hiring BOOLEAN DEFAULT FALSE
                       );
+
+
