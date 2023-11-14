5d9f964871e435f8754c690614432d9bcb59d64c
imerla <imerla@imerla.com>
2023-11-14 03:35:27 -0800

    update REDME and SQL script

---
+++ 02- Data Definition Managing Tables & Working With Data Types/create_employers_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employers(
+    company_name VARCHAR(250),
+    company_address VARCHAR(300),
+    -- yearly_revenue FLOAT(5, 2) Approximation
+    yearly_revenue NUMERIC(5, 2), -- exact value
+    is_hiring BOOLEAN
+                      );
