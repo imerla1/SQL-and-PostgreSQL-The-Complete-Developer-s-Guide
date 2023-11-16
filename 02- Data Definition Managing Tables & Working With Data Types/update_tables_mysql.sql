c88777b8ec473e76731c6a261b78e658fbeaad63
imerla <imerla@imerla.com>
2023-11-16 05:29:19 -0800

    update

--- 02- Data Definition Managing Tables & Working With Data Types/update_tables_mysql.sql
+++ 02- Data Definition Managing Tables & Working With Data Types/update_tables_mysql.sql
@@ -3,3 +3,9 @@ MODIFY COLUMN yearly_revenue FLOAT;
 
 ALTER TABLE users
 MODIFY COLUMN full_name VARCHAR(300);
+
+
+-- Add NOT null CONSTRAINT
+alter table users
+MODIFY column full_name VARCHAR(300) not null,
+MODIFY COLUMN status ENUM('employed', 'self-employed', 'unemployed') not null;
