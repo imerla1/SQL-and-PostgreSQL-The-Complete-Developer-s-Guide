11304c37f71c3a4936294bb8088a023116aaaa38
imerla <imerla@imerla.com>
2023-11-28 02:31:53 -0800

    update readme

--- 02- Data Definition Managing Tables & Working With Data Types/update_tables_pg.sql
+++ 02- Data Definition Managing Tables & Working With Data Types/update_tables_pg.sql
@@ -4,3 +4,13 @@ ALTER COLUMN yearly_revenue SET DATA TYPE FLOAT;
 
 ALTER TABLE users
 ALTER COLUMN full_name SET DATA TYPE VARCHAR(300);
+
+
+-- Add NOT null CONSTRAINT
+ALTER TABLE employers
+ALTER COLUMN company_address SET not null,
+ALTER COLUMN company_name SET NOT NULL;
+
+ALTER TABLE users
+ALTER column full_name set not null,
+ALTER COLUMN current_status set not null ;
