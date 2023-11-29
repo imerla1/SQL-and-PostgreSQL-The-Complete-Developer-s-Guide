193d1d4e16024abe904f0130cd8bfaf58c55ca7a
imerla <imerla@imerla.com>
2023-11-29 03:00:25 -0800

    update sql scripts

--- 02- Data Definition Managing Tables & Working With Data Types/01-create-table-mysql.sql
+++ 02- Data Definition Managing Tables & Working With Data Types/01-create-table-mysql.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE talently;
+# Create table
+CREATE TABLE users (
+    full_name VARCHAR(200),
+    yearly_salary INT,
+    status ENUM('employed', 'self-employed', 'unemployed') -- MySql Syntax
+);
--- 02- Data Definition Managing Tables & Working With Data Types/pg-create-table.sql
+++ 02- Data Definition Managing Tables & Working With Data Types/pg-create-table.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE talently;
+-- in pg we have to create CUSTOM type first for ENUM
+CREATE TYPE employment_status AS ENUM(
+    'employed',
+    'self-employed',
+    'unemployed'
+);
+
+CREATE TABLE users (
+    full_name VARCHAR(200),
+    yearly_salary INT
+
+    );
